91d8f3c71ae3143c79785112c0963d05de7cf1b9
supermannba <wangpengnba@gmail.com>
2015-08-19 19:39:42 -0700

    tcpunit test 1

--- lib/tcpunittest.py
+++ lib/tcpunittest.py
@@ -19,12 +19,7 @@ class TCPunittest:
 
 
 	def sendcommand(self,command):
-		# remoteip=self.tcp.sock.gethostbyname(Hostname.Remotehost.value)
-		# try:
-		# 	self.tcp.connect(remoteip,Tcpport.port1.value)
-		# except socket.error as e:
-		# 	print("error "+str(e))
-
+		
 		try:
 			self.tcp.senddata(command)
 		except socket.error as e:
@@ -51,12 +46,15 @@ class TCPunittest:
 if __name__=='__main__':
 
 	test1=TCPunittest()
-	remoteip=test1.tcp.sock.gethostbyname(Hostname.Remotehost.value)
+	remoteip=socket.gethostbyname(Hostname.Remotehost.value)
 	test1.tcp.connect(remoteip,Tcpport.port1.value)
 	command=b'Test Started'
 	test1.sendcommand(command)
-	resutl1=test1.receivedata()
-	print(result1)
+	test1.sendcommand(command)
+	host=''
+	data1=test1.servermode(host,time=1)
+	# result1=test1.receivedata()
+	print(data1)
 
 
 
